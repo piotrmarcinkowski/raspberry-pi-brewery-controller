18be8f93ab843f4aa92e136a209c499c1f0fd9ca
MARCINKOWSKI, PIOTR <piotr.marcinkowski@redembedded.com>
2019-01-21 00:48:05 +0100

    Add temperature reading for sensors

--- app/hardware/therm_sensor_api.py
+++ app/hardware/therm_sensor_api.py
@@ -1,11 +1,65 @@
 from w1thermsensor import W1ThermSensor
+import w1thermsensor.errors as w1errors
 
 
 class ThermSensorApi(object):
 
     def get_sensor_id_list(self):
+        """
+            Return IDs of all available sensors.
+
+            :returns: a list of sensor IDs.
+            :rtype: list
+        """
+
         w1_therm_sensors = W1ThermSensor.get_available_sensors()
         return tuple([x.id for x in w1_therm_sensors])
 
     def get_sensor_temperature(self, sensor_id):
-        return 0.0
+        """
+            Returns the temperature in celsius
+            :returns: the temperature in celsius
+            :rtype: float
+            :raises NoSensorFoundError: if the sensor with the given id could not be found
+            :raises SensorNotReadyError: if the sensor is not ready yet
+        """
+
+        w1_therm_sensors = W1ThermSensor.get_available_sensors()
+        for sensor in w1_therm_sensors:
+            if sensor.id == sensor_id:
+                try:
+                    return sensor.get_temperature()
+                except w1errors.NoSensorFoundError:
+                    raise NoSensorFoundError(sensor_id)
+                except w1errors.ResetValueError:
+                    raise SensorNotReadyError(sensor_id)
+                except w1errors.SensorNotReadyError:
+                    raise SensorNotReadyError(sensor_id)
+                except w1errors.W1ThermSensorError:
+                    raise ThermSensorError()
+
+        raise NoSensorFoundError(sensor_id)
+
+
+class ThermSensorError(Exception):
+    """Exception base-class for ThermSensor errors"""
+
+    pass
+
+
+class NoSensorFoundError(ThermSensorError):
+    """Exception when no sensor is found"""
+
+    def __init__(self, sensor_id):
+        super(NoSensorFoundError, self).__init__(
+            "No sensor with id '{0}' found".format(sensor_id)
+        )
+
+
+class SensorNotReadyError(ThermSensorError):
+    """Exception when the sensor is not ready yet"""
+
+    def __init__(self, sensor_id):
+        super(SensorNotReadyError, self).__init__(
+            "Sensor {} is not yet ready to read temperature".format(sensor_id)
+        )
--- tests/unit/hardware/therm_sensor_api_test.py
+++ tests/unit/hardware/therm_sensor_api_test.py
@@ -2,30 +2,67 @@ import unittest
 from unittest.mock import Mock
 
 from w1thermsensor import W1ThermSensor
+import w1thermsensor.errors as w1errors
 
 from app.hardware.therm_sensor_api import ThermSensorApi
+from app.hardware.therm_sensor_api import NoSensorFoundError, SensorNotReadyError, ThermSensorError
 
 
 class ThermSensorApiTestCase(unittest.TestCase):
+    MOCKED_SENSORS = [
+        {'id': '100001', 'temp': 35.1},
+        {'id': '100002', 'temp': 36.2},
+        {'id': '100003', 'temp': 37.9},
+    ]
+
+    def setUp(self):
+        self.mocked_sensors = []
+        for mock_data in self.MOCKED_SENSORS:
+            sensor = Mock(spec=W1ThermSensor)
+            sensor.id = mock_data['id']
+            sensor.get_temperature = Mock(return_value=mock_data['temp'])
+            self.mocked_sensors.append(sensor)
+        W1ThermSensor.get_available_sensors = Mock(return_value=self.mocked_sensors)
 
     def test_should_return_proper_id_list(self):
-        sensor1 = Mock(spec=W1ThermSensor)
-        sensor1.id = '100001'
-        sensor2 = Mock(spec=W1ThermSensor)
-        sensor2.id = '100002'
-        sensor3 = Mock(spec=W1ThermSensor)
-        sensor3.id = '100003'
-        mocked_sensors = [sensor1, sensor2, sensor3]
-        W1ThermSensor.get_available_sensors = Mock(return_value=mocked_sensors)
-
         api = ThermSensorApi()
         detected_sensors = api.get_sensor_id_list()
 
         W1ThermSensor.get_available_sensors.assert_called_once()
-        self.assertEqual(len(detected_sensors), len(mocked_sensors))
-        self.assertEqual(detected_sensors[0], '100001')
-        self.assertEqual(detected_sensors[1], '100002')
-        self.assertEqual(detected_sensors[2], '100003')
+        self.assertEqual(len(detected_sensors), len(self.MOCKED_SENSORS))
+        for index in range(len(self.MOCKED_SENSORS)):
+            self.assertEqual(detected_sensors[index], self.MOCKED_SENSORS[index]['id'])
+
+    def test_should_return_temperature_for_sensor(self):
+        api = ThermSensorApi()
+
+        for index in range(len(self.MOCKED_SENSORS)):
+            temp = api.get_sensor_temperature(self.MOCKED_SENSORS[index]['id'])
+            self.assertEqual(temp, self.MOCKED_SENSORS[index]['temp'])
+
+    def test_should_raise_error_when_sensor_not_found(self):
+        api = ThermSensorApi()
+        with self.assertRaises(NoSensorFoundError):
+            api.get_sensor_temperature("invalid_id")
+
+    def test_should_raise_error_when_lib_raised_error(self):
+        api = ThermSensorApi()
+        self.mocked_sensors[0].get_temperature = Mock(side_effect=w1errors.NoSensorFoundError("name", "id"))
+        self.mocked_sensors[1].get_temperature = Mock(side_effect=w1errors.SensorNotReadyError(self.mocked_sensors[1]))
+        self.mocked_sensors[2].get_temperature = Mock(side_effect=w1errors.ResetValueError(self.mocked_sensors[1]))
+
+        with self.assertRaises(NoSensorFoundError):
+            api.get_sensor_temperature(self.MOCKED_SENSORS[0]["id"])
+
+        with self.assertRaises(SensorNotReadyError):
+            api.get_sensor_temperature(self.MOCKED_SENSORS[1]["id"])
+
+        with self.assertRaises(SensorNotReadyError):
+            api.get_sensor_temperature(self.MOCKED_SENSORS[2]["id"])
+
+        self.mocked_sensors[0].get_temperature = Mock(side_effect=w1errors.KernelModuleLoadError())
+        with self.assertRaises(ThermSensorError):
+            api.get_sensor_temperature(self.MOCKED_SENSORS[0]["id"])
 
 
 if __name__ == '__main__':
