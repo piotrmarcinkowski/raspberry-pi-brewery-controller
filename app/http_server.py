ca438bbfe9d779910a0072ab1c331a92d5a192ca
MARCINKOWSKI, PIOTR <piotr.marcinkowski@redembedded.com>
2019-02-10 17:49:32 +0100

    Set server port to 80 (previously 5000)

--- app/http_server.py
+++ app/http_server.py
@@ -10,6 +10,7 @@ this_module = sys.modules[__name__]
 __controller = None
 __server_running = False
 app = Flask("BreweryRestAPI")
+SERVER_PORT = 80
 URL_PATH = "/brewery/api/v1.0/"
 URL_RESOURCE_SENSORS = "therm_sensors"
 
@@ -26,7 +27,7 @@ def get_therm_sensors():
 def start_server():
     if this_module.__server_running:
         raise RuntimeError("Server already running")
-    app.run(debug=False, use_reloader=False)
+    app.run(debug=False, use_reloader=False, port=SERVER_PORT)
 
 
 def start_server_in_separate_thread():
