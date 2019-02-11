162407d3478d8647f1697a7940b9e921cf2c76e5
MARCINKOWSKI, PIOTR <piotr.marcinkowski@redembedded.com>
2019-02-11 19:13:56 +0100

    Make flask server available for others in lan

--- app/http_server.py
+++ app/http_server.py
@@ -10,6 +10,7 @@ this_module = sys.modules[__name__]
 __controller = None
 __server_running = False
 app = Flask("BreweryRestAPI")
+SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 80
 URL_PATH = "/brewery/api/v1.0/"
 URL_RESOURCE_SENSORS = "therm_sensors"
@@ -27,7 +28,7 @@ def get_therm_sensors():
 def start_server():
     if this_module.__server_running:
         raise RuntimeError("Server already running")
-    app.run(debug=False, use_reloader=False, port=SERVER_PORT)
+    app.run(debug=False, use_reloader=False, host=SERVER_HOST, port=SERVER_PORT)
 
 
 def start_server_in_separate_thread():
