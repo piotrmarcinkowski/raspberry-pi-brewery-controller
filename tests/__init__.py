093ad449c7266f8981eb1c8247525ab4d33cfd50
Piotr Marcinkowski <piotrmarc@gmail.com>
2020-11-11 15:03:26 +0100

    Fix unit tests RPi.GPIO module visibility

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,7 @@
 import os
+import sys
 print("tests __init__.py")
 os.environ['W1THERMSENSOR_NO_KERNEL_MODULE'] = '1'
+# Add tests dir to PYTHONPATH to make the tests/RPi module mock available in tests
+sys.path.insert(0, os.path.join(os.getcwd(), "tests"))
+print("sys.path=" + str(sys.path))
